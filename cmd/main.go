03c90fa144be7bf780a82138b36a6b92b082089f
hiamthach108 <dreon1008@gmail.com>
2025-09-30 21:25:11 +0700

    chore: init project folders

---
+++ cmd/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// This is the main entry point of the application.
+	// Add your application logic here.
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hiamthach108/simplerank
+
+go 1.25.0
